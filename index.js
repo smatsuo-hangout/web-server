a5367d34381a2225d04cebc7c4112cf95923783c
Suguru Matsuo <smatsuo.contact@gmail.com>
2026-04-14 13:57:51 +0900

    Herokuエラー対応2

--- index.js
+++ index.js
@@ -13,14 +13,11 @@ app.use(express.static(path.join(__dirname, "public")))
 app.post('/api/v1/quiz', (req, res) => {
   const answer = req.body.answer;
   if(answer === "2") {
-    // res.send("<h1>正解！</h1>")
-    res.redirect("/correct.html");
+    return res.redirect("/correct.html");
   } else {
-    // res.send("<h1>不正解…</h1>");
-    res.redirect("/wrong.html");
+    return res.redirect("/wrong.html");
   }
-  res.send(answer);
-})
+});
 
 app.get('/api/v1/users', (req, res) => {
   res.send({
