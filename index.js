e1a8149672bf43b4ad96d2cdb7f1ca1a55a70dd7
Suguru Matsuo <smatsuo.contact@gmail.com>
2026-04-14 13:31:07 +0900

    Heroku対応

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.get('/api/v1/users', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running')
 })
